cfe5280d4ab1262e9a1a4911c9981316de7808d8
serikzhankomekov <serikzhan.komekov@homecredit.kz>
2023-01-13 00:06:50 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(View());
 }
 
+class View extends StatelessWidget {
+  const View({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
