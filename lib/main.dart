98f207465819861c4ccc4d3368313fab38323a88
serikzhankomekov <serikzhan.komekov@homecredit.kz>
2023-01-23 00:18:23 +0600

    Final Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(View());
@@ -7,12 +8,43 @@ void main() {
 class View extends StatelessWidget {
   const View({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+
+  Expanded buildUI(Color colorButton, soundNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+         playSound(soundNumber);
+        },
+        child: Text(''),
+        style: TextButton.styleFrom(backgroundColor: colorButton),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildUI(Colors.red, 1),
+              buildUI(Colors.yellow, 2),
+              buildUI(Colors.orange, 3),
+              buildUI(Colors.blue, 4),
+              buildUI(Colors.green, 5),
+              buildUI(Colors.greenAccent, 6),
+              buildUI(Colors.pink, 7),
+            ],
+          ),
         ),
       ),
     );
